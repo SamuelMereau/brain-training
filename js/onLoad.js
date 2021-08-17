c58e950b58ae3eb10daa47827be447e096e7d5f4
SamuelMereau <mereau_s@students.gpcc.nsw.edu.au>
2021-08-17 19:18:14 +1000

    add start button
    listen to stuff yay

--- js/onLoad.js
+++ js/onLoad.js
@@ -1,12 +1,39 @@
 window.onload = function () {
-    let intro = new Audio('../audio/intro.mp3');
-    intro.volume = 0.3;
-    intro.play();
-    let loop = new Audio('../audio/loop.mp3');
-    loop.volume = 0.3;
-    loop.loop = true;
-    intro.addEventListener("ended", function () {
-        loop.play();
+    const div = document.getElementById("loading");
+    const button = document.getElementById("start");
+    button.addEventListener("click", function () {
+        // Create loading bars
+        const progressBar = document.createElement("div");
+        progressBar.id = "progress-bar";
+        const progress = document.createElement("div");
+        progress.id = "progress";
+        progressBar.appendChild(progress);
+        const brainAge = document.createElement("p");
+        brainAge.id = "brain-age";
+        // Append to loading div
+        div.appendChild(progressBar);
+        div.appendChild(brainAge);
+        // Change heading
+        const heading = document.getElementById("heading");
+        heading.innerHTML = "Training the brain...";
+        // Load loadingBar.js
+        let script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "./js/loadingBar.js"; 
+        document.body.appendChild(script);
+        // Remove button
+        button.remove();
+        // Play music
+        let intro = new Audio('../audio/intro.mp3');
+        intro.volume = 0.3;
+        intro.play();
+        let loop = new Audio('../audio/loop.mp3');
+        loop.volume = 0.3;
+        loop.loop = true;
+        intro.addEventListener("ended", function () {
+            loop.play();
+        })
+        return false;
     })
     console.log("https://github.com/SamuelMereau/brain-training");
 }
